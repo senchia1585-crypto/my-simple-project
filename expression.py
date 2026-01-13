d4a4b2e5b9d16f68c4bbd62c93bc4d08884c375e
senchia1585-crypto <senchia1585@gmail.com>
2026-01-13 21:31:38 +0800

    Add files via upload

---
+++ expression.py
@@ -0,0 +1,18 @@
+# programmer intro to expression
+# interger Is the number ever that is 1000 /-1000 that is call interger
+print(2+100) # interger can + by print commad
+print(100.900 + 100.00) # this call float that means that is number with decimal point
+print("Hello"+ " " + "World")
+# "" for string that means use this "" mark anything can be string 
+# string cannot add with number  will become logic error
+# True and False can be dentify in boolean  that is an unit or special thing in code
+
+print(True)
+print(False)
+x=1
+x+1=1
+If x==10:
+
+    
+
+
